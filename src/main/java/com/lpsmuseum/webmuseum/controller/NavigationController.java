2c9c337a2a85af3cc1f7330e0dc16bb510634684
Guilherme <guijocargo@live.com>
2015-07-31 20:49:47 -0300

    Mais um commit antes de fazer alguns testes com jenkins e git, só falta navegação com desafiooooo!!!! =/

--- src/main/java/com/lpsmuseum/webmuseum/controller/NavigationController.java
+++ src/main/java/com/lpsmuseum/webmuseum/controller/NavigationController.java
@@ -3,12 +3,14 @@ package com.lpsmuseum.webmuseum.controller;
 import com.lpsmuseum.behaviour.museum.navigation.AleatoryNavigation;
 import com.lpsmuseum.behaviour.museum.navigation.GuidedNavigation;
 import com.lpsmuseum.behaviour.museum.navigation.Node;
+import com.lpsmuseum.dto.Museum;
 import com.lpsmuseum.dto.Scenario;
 import com.lpsmuseum.dto.scenario.Challenge;
 import com.lpsmuseum.service.ChallengePastPresentService;
 import com.lpsmuseum.service.ChallengeService;
 import com.lpsmuseum.service.MuseumService;
 import com.lpsmuseum.service.ScenarioService;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +23,17 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class NavigationController {
 
+    private static Museum museum;
+    private static List<Challenge> challenges;
+    private static List<Challenge> allChallenges;
     private static Node noatual;
     private static int indexAtual;
+
+    public NavigationController() {
+        challenges = new ArrayList<>();
+        allChallenges = new ChallengeService().listChallenges();
+        allChallenges.addAll(new ChallengePastPresentService().listChallenges());
+    }
 
     @RequestMapping("navigation")
     public ModelAndView chooseMuseum() {
@@ -33,103 +44,58 @@ public class NavigationController {
 
     @RequestMapping("navigation/navigate")
     public ModelAndView navigateMuseum(Long id, String navigationMode) {
-        ModelAndView mv = null;
+        ModelAndView mv = new ModelAndView("navigation/scenario");
+        
+        museum = new MuseumService().findById(id);
+        indexAtual = 0;
         if (null != navigationMode) {
             switch (navigationMode) {
                 case "Aleatory":
-                    mv = new ModelAndView("navigation/aleatory");
-                    break;
+                    noatual = new AleatoryNavigation().getNavigation(museum.getScenarios());
                 case "Guided":
-                    mv = new ModelAndView("navigation/guided");
-                    break;
+                    noatual = new GuidedNavigation().getNavigation(museum.getScenarios());
             }
         }
-        if (mv != null)
-            mv.addObject("museum", new MuseumService().findById(id));
-        return mv;
-    }
-
-    @RequestMapping("navigation/aleatory")
-    public ModelAndView aleatorio() {
-        indexAtual = 0;
-        noatual = new AleatoryNavigation().getNavigation(new ScenarioService().listScenarios());
-        ModelAndView mv = new ModelAndView("navigation/aleatory");
-        Scenario scenario = noatual.getNeighbors().get(indexAtual).getScenario();
-        mv.addObject("scenario", scenario);
-        List<Challenge> challenges = new ChallengeService().listChallenges();
-        challenges.addAll(new ChallengePastPresentService().listChallenges());
-        boolean hasChallenge = false;
-        for (Challenge challenge : challenges) {
-            if (challenge.getScenario().getId().equals(scenario.getId())) {
-                hasChallenge = true;
-                break;
-            }
-        }
-        mv.addObject("hasChallenge", hasChallenge);
-        mv.addObject("museum", new MuseumService().listMuseum().get(0));
-        return mv;
-    }
-
-    @RequestMapping("navigation/guided")
-    public ModelAndView guiado() {
-        indexAtual = 0;
-        noatual = new GuidedNavigation().getNavigation(new ScenarioService().listScenarios());
-        ModelAndView mv = new ModelAndView("navigation/guided");
-        Scenario scenario = noatual.getNeighbors().get(indexAtual).getScenario();
-        mv.addObject("scenario", scenario);
-        List<Challenge> challenges = new ChallengeService().listChallenges();
-        challenges.addAll(new ChallengePastPresentService().listChallenges());
-        boolean hasChallenge = false;
-        for (Challenge challenge : challenges) {
-            if (challenge.getScenario().getId().equals(scenario.getId())) {
-                hasChallenge = true;
-                break;
-            }
-        }
-        mv.addObject("hasChallenge", hasChallenge);
-        mv.addObject("museum", new MuseumService().listMuseum().get(0));
+        
+        mv.addObject("scenario", noatual.getNeighbors().get(indexAtual).getScenario());
+        mv.addObject("hasChallenge", hasChallenges());
+        mv.addObject("museum", museum.getName());
+        mv.addObject("scenario", noatual.getScenario());
+        
         return mv;
     }
 
     @RequestMapping("navigation/next")
     public ModelAndView avancar() {
         ModelAndView mv = new ModelAndView("navigation/scenario");
+        
         indexAtual++;
         if (indexAtual >= noatual.getNeighbors().size()) {
             indexAtual = 0;
         }
-        Scenario scenario = noatual.getNeighbors().get(indexAtual).getScenario();
-        mv.addObject("scenario", scenario);
-        List<Challenge> challenges = new ChallengeService().listChallenges();
-        boolean hasChallenge = false;
-        for (Challenge challenge : challenges) {
-            if (challenge.getScenario().equals(scenario.getId())) {
-                hasChallenge = true;
-                break;
-            }
-        }
-        mv.addObject("hasChallenge", hasChallenge);
+        
+        mv.addObject("scenario", noatual.getNeighbors().get(indexAtual).getScenario());
+        mv.addObject("hasChallenge", hasChallenges());
+        mv.addObject("museum", museum.getName());
+        mv.addObject("scenario", noatual.getScenario());
+        
         return mv;
     }
 
     @RequestMapping("navigation/back")
     public ModelAndView voltar() {
         ModelAndView mv = new ModelAndView("navigation/scenario");
+        
         indexAtual--;
         if (indexAtual < 0) {
             indexAtual = noatual.getNeighbors().size() - 1;
         }
-        Scenario scenario = noatual.getNeighbors().get(indexAtual).getScenario();
-        mv.addObject("scenario", scenario);
-        List<Challenge> challenges = new ChallengeService().listChallenges();
-        boolean hasChallenge = false;
-        for (Challenge challenge : challenges) {
-            if (challenge.getScenario().equals(scenario.getId())) {
-                hasChallenge = true;
-                break;
-            }
-        }
-        mv.addObject("hasChallenge", hasChallenge);
+        
+        mv.addObject("scenario", noatual.getNeighbors().get(indexAtual).getScenario());
+        mv.addObject("hasChallenge", hasChallenges());
+        mv.addObject("museum", museum.getName());
+        mv.addObject("scenario", noatual.getScenario());
+        
         return mv;
     }
 
@@ -145,5 +111,16 @@ public class NavigationController {
         }
         mv.addObject("scenarioId", scenarioId);
         return mv;
+    }
+    
+    private boolean hasChallenges() {
+        if (!NavigationController.challenges.isEmpty())
+            NavigationController.challenges.clear();
+        
+        for (Challenge challenge : challenges)
+            if (challenge.getScenario().getId().equals(noatual.getScenario().getId()))
+                NavigationController.challenges.add(challenge);
+        
+        return NavigationController.challenges.isEmpty();
     }
 }
